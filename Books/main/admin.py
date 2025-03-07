a9f186b2d51fd77632edfdd1631450b4da299229
Samandar Nemataliyev <samandarnemataliyev@gmail.com>
2025-03-07 11:42:37 +0500

    admin qismida o'zgarishlar qilindi

--- Books/main/admin.py
+++ Books/main/admin.py
@@ -3,4 +3,10 @@ from .models import Book
 
 # Register your models here.
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'genre', 'publisher', 'price', 'quantity')
+    search_fields = ('title', 'author', 'genre', 'publisher')
+    list_filter = ('title', 'author', 'genre', 'publisher')
+    ordering = ('id',)
+
+admin.site.register(Book, BookAdmin)
